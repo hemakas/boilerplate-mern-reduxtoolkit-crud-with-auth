7d6d8ce291f7ad7f99699fa3198473e4f425c506
hemakas <hemaka.tropusi@gmail.com>
2022-09-09 12:57:18 +0530

    models added

---
+++ backend/models/Event.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+const eventSchema = mongoose.Schema(
+  {
+    // foreign key
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User',
+    },
+    // googleId: {
+    //     type: String,
+    //     required: true
+    // }, 
+    title: {
+      type: String,
+      required: [true, 'Please add a title'],
+    },
+    description: {
+      type: String,
+      required: [true, 'Please add an email'],
+      unique: true,
+    },
+    start: {
+        type: Date,
+        required: [true, 'Please add an start date'],
+    },
+    end: {
+        type: Date,
+        required: [true, 'Please add end date'],
+    },
+    
+  }, { timestamps: true })
+
+module.exports = mongoose.model('Event', eventSchema)
