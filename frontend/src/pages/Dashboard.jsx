d8d8507d8ef593492ef525376eb83270ce35b44a
hemakas <hemaka.tropusi@gmail.com>
2022-09-20 18:48:28 +0530

    restricting dashboard access for non users

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,20 @@
-import Alert from 'react-bootstrap/Alert';
+import Alert from 'react-bootstrap/Alert'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'  
 
 function Dashboard() {
+  const navigate = useNavigate()
+  const { user } = useSelector((state) => state.auth)
+
+  useEffect(() => {
+   
+    if (!user) {
+      navigate('/login')
+    }
+    
+  }, [user, navigate])
+
   return (
     <>
       <Alert variant="success" className='mt-3'>
